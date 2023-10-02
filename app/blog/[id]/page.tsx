a6e7f9065ffa67bf35115b4abf8e403b76c60f05
TERtISmu <hERomaCe@proton.me>
2023-10-03 01:18:31 +0300

    feat: add metadata in pages

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,18 @@
+import { Metadata } from "next";
+
 type Props = {
   params: {
     id: string;
   };
 };
+
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  return {
+    title: `${id} | Next App`,
+  };
+}
 
 export default function Post({ params: { id } }: Props) {
   return <h1>Blog page{id}</h1>;
